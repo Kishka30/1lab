8d50f94ba7659c96647e088f9ab9d91076d36ba6
idktokillmyself <romanmoroz04082006@icloud.com>
2025-03-06 08:43:21 -0500

    My code for first lab

---
+++ roma/Znaki.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string input = "For example: apple, banana, meat.";
+    std::string cleaned = ""; // Создаем пустую строку для результата
+
+    // Проходим по каждому символу в строке input
+    for (int i = 0; i < input.length(); i++) {
+        char c = input[i]; // Берем текущий символ
+
+        // Если символ - буква или пробел, добавляем его в cleaned
+        if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == ' ') {
+            cleaned += c; // Добавляем символ в очищенную строку
+        }
+    }
+
+    std::cout << cleaned << std::endl; // Выводим очищенную строку
+
+    return 0;
+}
